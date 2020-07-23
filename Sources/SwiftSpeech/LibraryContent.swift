d482611cccfdcbd39c7dc3abfb1554f5ff0e124b
Cay Zhang <zcy20020811@gmail.com>
2020-07-22 20:38:29 -0700

    LibraryContent

---
+++ Sources/SwiftSpeech/LibraryContent.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cay Zhang on 2020/7/22.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct LibraryContent: LibraryContentProvider {
+    @LibraryContentBuilder
+    var views: [LibraryItem] {
+        LibraryItem(
+            SwiftSpeech.RecordButton(),
+            title: "Record Button"
+        )
+        
+        LibraryItem(
+            SwiftSpeech.Demos.Basic(locale: .current),
+            title: "Demo - Basic"
+        )
+        
+        LibraryItem(
+            SwiftSpeech.Demos.Colors(),
+            title: "Demo - Colors"
+        )
+        
+        LibraryItem(
+            SwiftSpeech.Demos.List(locale: .current),
+            title: "Demos - List"
+        )
+    }
+}
